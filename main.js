708ce19d8b07508ef6ddc96d5be8e5c46af06118
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-05-20 13:54:16 -0300

    20.05

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const botoes = document.querySelectorAll(".botao");
 
 for (let i=0; i< botoes.length; i++){
     botoes[i].onclick = function () {
-        botoes[i].classList.add(".ativo")
+        botoes[i].classList.add("ativo")
     };
 }
