327264958b49f25bb00b9b7821ce2715aa1aba10
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-06-03 14:26:17 -0300

    03.06

--- main.js
+++ main.js
@@ -18,4 +18,4 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent =tempoobjetivo1-tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
