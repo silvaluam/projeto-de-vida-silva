47fdc0a5b9a0c809f9c5a11115c6a894caf1543e
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-06-03 14:22:54 -0300

    03.06

--- main.js
+++ main.js
@@ -15,3 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent =tempoobjetivo1-tempoAtual;
