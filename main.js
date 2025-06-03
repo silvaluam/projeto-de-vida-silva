5dc0214c9385d9a76a51c5801b0eaede7d1becab
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-06-03 14:54:12 -0300

    03.06

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ contadores[0].textContent = calculatempo(tempoObjetivo1);
 
 function calculatempo(tempoObjetivo) {
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo-temporal;
+    let tempoFinal = tempoObjetivo-tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
